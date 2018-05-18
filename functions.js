80df64cc0b60409e3be9853a6a102d5728cd8a6b
Alex <k9o8@ugrad.cs.ubc.ca>
2018-05-17 23:02:19 -0700

    proof of concept working
    need to implement JS to automatically generate from JSON file.

---
+++ functions.js
@@ -0,0 +1,14 @@
+
+function getHighlights(){
+	var highlightsJSON = "https://goo.gl/3QixSY"
+	$.getJSON(highlightsJSON, function(json){
+		var html = "";
+		var hightlightID = 1;
+		$.forEach(json, function(key,value){
+			html += '<div id="highlight' + highlightID + 
+		
+	
+	});
+	
+	});
+}
