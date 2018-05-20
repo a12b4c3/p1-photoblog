ecc8dfd51a1941e92995f4a5b86b2f7ae7d94406
Alex <k9o8@ugrad.cs.ubc.ca>
2018-05-20 01:27:44 -0700

    trial remove html status check

--- functions.js
+++ functions.js
@@ -82,7 +82,7 @@ function getHighlights(){
 	var highlightsJSON = "highlights.json";
 	var xmlhttp = new XMLHttpRequest();
 	xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
+    if (true) { //this.readyState == 4 && this.status == 200
         var myArr = JSON.parse(this.responseText);
 		// var html = "";
 		var currentElement = "";
